967658c4caa3e6334a41f1758593dc1b8505789c
freeleeman <62164480+freeleeman@users.noreply.github.com>
2020-08-12 17:43:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #应用过滤插件
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 #KoolProxyR去广告插件
