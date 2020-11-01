57aa0c8bb80e13a39a02d59c8e7520547b59c0ee
freeleeman <62164480+freeleeman@users.noreply.github.com>
2020-11-01 11:35:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,5 +16,5 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 # Modify default PassWord
 # sed -i 's/root::0:0:99999:7:::/root:$1$ScQIGKsX$q0qEf\/tAQ2wpTR6zIUIjo.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-sed -i '7{s/$/ R20.9.15 | by woodfree/}' ./package/base-files/files/etc/banner
-sed -i "s/DISTRIB_REVISION='R20.9.15'/DISTRIB_REVISION='R20.9.15 | by woodfree'/g" ./package/lean/default-settings/files/zzz-default-settings
+sed -i '7{s/$/ R20.10.20 | by woodfree/}' ./package/base-files/files/etc/banner
+sed -i "s/DISTRIB_REVISION='R20.9.15'/DISTRIB_REVISION='R20.10.20 | by woodfree'/g" ./package/lean/default-settings/files/zzz-default-settings
