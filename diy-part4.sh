12fbe9479e894071086ddc34c2667475dcac6e00
freeleeman <62164480+freeleeman@users.noreply.github.com>
2020-11-11 13:21:46 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -17,4 +17,4 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 # sed -i 's/root::0:0:99999:7:::/root:$1$ScQIGKsX$q0qEf\/tAQ2wpTR6zIUIjo.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 sed -i '7{s/$/ R20.7.1 | by woodfree/}' ./package/base-files/files/etc/banner
-sed -i "s/DISTRIB_REVISION='R20.8.27'/DISTRIB_REVISION='R20.7.1 | by woodfree'/g" ./package/lean/default-settings/files/zzz-default-settings
+sed -i "s/DISTRIB_REVISION='R20.7.1'/DISTRIB_REVISION='R20.7.1 | by woodfree'/g" ./package/lean/default-settings/files/zzz-default-settings
